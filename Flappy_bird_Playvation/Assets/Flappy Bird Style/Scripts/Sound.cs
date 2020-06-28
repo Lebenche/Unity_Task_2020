0177f90f73c195390e3be23b9bf8d79ac1d2e34d
Lenny <lenbencherif@gmail.com>
2020-06-28 20:15:42 +0200

    Importing sounds + creating an audio manager
    - Sounds from original game were imported and placed in a folder called "Sounds"
    - A Gameobject "AudioManager" was created to add and manage sounds easier.

---
+++ Flappy_bird_Playvation/Assets/Flappy Bird Style/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+  public AudioClip clip;
+  public float volume;
+  public float pitch;
+    // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+    // Update is called once per frame
+  void Update()
+  {
+        
+  }
+}
