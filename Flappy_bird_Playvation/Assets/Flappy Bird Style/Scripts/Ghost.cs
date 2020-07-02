3f9ce7c0ed4d4cc8bf616c9448260ec09c6ecc8d
Lenny <lenbencherif@gmail.com>
2020-07-02 21:25:37 +0200

    [func]Starting the creation of a bonus power up "Ghost"
    With this bonus bird can go through obstacles during an amount of time.
    To make this happen I modified the column prefab by increasing the size of the collider which triggered the score. I also created a gameobject with a ghost sprite. When the bird collides with this gameobject his trigger parameter in the collider component switches to true and his sprite becomes transparent.

---
+++ Flappy_bird_Playvation/Assets/Flappy Bird Style/Scripts/Ghost.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public GameObject bird;
+  private bool isBonusActive = false;
+  float timer = 0;
+    void Start()
+    {
+        
+    }
+
+  private void Update()
+  {
+    if (isBonusActive) {
+      timer += Time.deltaTime;
+      Debug.Log(timer);
+    }
+    if (timer >= 4.0f) {
+      LeanTween.alpha(bird, 1f, 1f).setOnComplete(BonusEnd);
+      timer = 0f;
+
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.GetComponent<Bird>() != null) {
+      //If the bird hits the trigger collider of the ghost bonus 
+      //
+
+      bird.GetComponent<Collider2D>().isTrigger = true;
+      LeanTween.alpha(bird, 0.6f, 1f);
+      isBonusActive = true;
+    }
+  }
+  void BonusEnd()
+  {
+    isBonusActive = false;
+    bird.GetComponent<Collider2D>().isTrigger = false;
+
+    
+  }
+}
