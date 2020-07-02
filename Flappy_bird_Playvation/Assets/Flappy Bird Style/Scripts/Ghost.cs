3d495c8101fa6bdede5164619c1dfb8c30b4fcdb
Lenny <lenbencherif@gmail.com>
2020-07-02 22:55:20 +0200

    [edit] enhancing Ghost Bonus
    Now the Ghost bonus icon appears at random positions many times during the game without touching obstacles so the bird can reach it.

--- Flappy_bird_Playvation/Assets/Flappy Bird Style/Scripts/Ghost.cs
+++ Flappy_bird_Playvation/Assets/Flappy Bird Style/Scripts/Ghost.cs
@@ -8,40 +8,63 @@ public class Ghost : MonoBehaviour
   public GameObject bird;
   private bool isBonusActive = false;
   float timer = 0;
-    void Start()
-    {
-        
-    }
+  public float bonusDuration=4;
+  void Start()
+  {
+    MoveBonus();   
+  }
 
   private void Update()
   {
     if (isBonusActive) {
       timer += Time.deltaTime;
-      Debug.Log(timer);
     }
-    if (timer >= 4.0f) {
+
+    if (timer >= bonusDuration) {
       LeanTween.alpha(bird, 1f, 1f).setOnComplete(BonusEnd);
       timer = 0f;
 
+    }
+    // If the bonus hasn't been catched by the bird, he moves forward 
+    if (gameObject.transform.position.x < bird.transform.position.x-7) {
+      MoveBonus();
     }
   }
 
   void OnTriggerEnter2D(Collider2D other)
   {
     if (other.GetComponent<Bird>() != null) {
-      //If the bird hits the trigger collider of the ghost bonus 
+      //If the bird hits the trigger collider of the ghost bonus ....
       //
-
+      //... its trigger becomes true
       bird.GetComponent<Collider2D>().isTrigger = true;
+      // it becomes transparent 
       LeanTween.alpha(bird, 0.6f, 1f);
       isBonusActive = true;
+      // ...the bonus move 
+
+      bird.GetComponent<Rigidbody2D>().freezeRotation = true;
+      MoveBonus();
+    }
+
+    // If the bonus touches an obstacle, the bonus changes its position
+    if (other.GetComponent<BoxCollider2D>() != null && other.tag!="Player") {
+      LeanTween.move(gameObject, new Vector3(gameObject.transform.position.x + 2, Random.Range(-1, 4), 0), 0f);
+      Debug.Log("Object moved");
     }
   }
+
+  // When the bonus end bird's parameters return to normal 
   void BonusEnd()
   {
     isBonusActive = false;
     bird.GetComponent<Collider2D>().isTrigger = false;
+    bird.GetComponent<Rigidbody2D>().freezeRotation = false;
 
-    
+  }
+  // Change the bonus position so the bird can catch it an other time 
+  void MoveBonus() {
+    LeanTween.move(gameObject, new Vector3(Random.Range(10, 35), Random.Range(-1, 4), 0), 0f);
+
   }
 }
